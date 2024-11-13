0e0c782ee082e1b7fdee490eac116239198c7108
user <tavabilovrr@gmail.com>
2024-11-13 23:40:10 +0900

    q_v3

--- pythonProject1/q.py
+++ pythonProject1/q.py
@@ -2,4 +2,4 @@ number_of_HW_completed = 12
 number_of_hours_spent = 1.5
 lesson_title = 'Python'
 time_for_one_task = number_of_hours_spent / number_of_HW_completed
-print('Курс: ' + lesson_title + ', всего задач: '+ str(number_of_HW_completed) + ', затрачено часов: ' + str(number_of_hours_spent) + ', среднее время выполнения: ' + str(time_for_one_task) + ' часа')
+print('Курс:' + lesson_title + ', всего задач:'+ str(number_of_HW_completed) + ', затрачено часов:' + str(number_of_hours_spent) + ', среднее время выполнения:' + str(time_for_one_task) + ' часа')
