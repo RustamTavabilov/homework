6e9a40146a5a1599ef1300f9183c6e03e29a5fef
user <tavabilovrr@gmail.com>
2024-11-17 16:34:42 +0900

    Sredniy_ball

---
+++ pythonProject1/Sredniy_ball.py
@@ -0,0 +1,7 @@
+grades = [[5,3,3,5,4] , [2,2,2,3] , [4,5,5,2] , [4,4,2] , [5,5,5,4,5]]
+students = {'Johnny' , 'Bildo' , 'Steve' , 'Khendrik' , 'Aaron'}
+students = sorted(students)
+grades_sred=[sum(grades)/len(grades) for grades in grades]
+a = dict(zip(students,grades_sred))
+
+print(a)
---
+++ pythonProject1/w.py
@@ -0,0 +1,6 @@
+example = 'Автоботы'
+print(example[0])
+print(example[-1])
+print(example[4:8])
+print(example[::-1])
+print(example[1::2])
