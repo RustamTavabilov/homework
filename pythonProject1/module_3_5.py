ddad0c27c2133adec3963c39beb44389debad2fe
user <tavabilovrr@gmail.com>
2024-12-14 20:18:05 +0900

    module_3_5_v2

--- pythonProject1/module_3_5.py
+++ pythonProject1/module_3_5.py
@@ -4,8 +4,10 @@ def get_multiplied_digits(namber):
 
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
+    elif first == 0:
+        return 1
+    return first
+
 
 result = get_multiplied_digits(40203)
 print(result)
