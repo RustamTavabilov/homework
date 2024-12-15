36a0be0efcf8a812e6610aee9207d42d8822458d
user <tavabilovrr@gmail.com>
2024-12-15 18:00:04 +0900

    module_4_1_v1

---
+++ pythonProject1/module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fake_del
+import true_math as true_del
+
+result1 = fake_del.divide(69, 3)
+result2 = fake_del.divide(3, 0)
+result3 = true_del.divide(49, 7)
+result4 = true_del.divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
