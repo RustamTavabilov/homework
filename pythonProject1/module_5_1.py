5afc6c573ed1dd5459287a16bf7218b030c34418
user <tavabilovrr@gmail.com>
2024-12-21 22:06:29 +0900

    module_5_1_v1

---
+++ pythonProject1/module_5_1.py
@@ -0,0 +1,20 @@
+class House:
+    def __init__(self, name, namber_of_floor):
+        self.name = name
+        self.namber_of_floor = namber_of_floor
+
+
+
+    def go_to(self, new_floor):
+        if self.namber_of_floor > new_floor:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('Такого этажа не существует')
+
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
