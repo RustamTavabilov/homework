22b03ed4717cf9cf1485ef84b3c738f2e8a142a8
user <tavabilovrr@gmail.com>
2024-12-22 17:42:07 +0900

    module_5_2_v2

---
+++ pythonProject2/ins.py
@@ -0,0 +1,15 @@
+from encodings.punycode import insertion_sort
+
+from  module_4.sortfuns import *
+
+data_1 = list(map(int, input('Введите числа через пробел ').split()))
+data_2 = list(map(int, input('Введите числа через пробел ').split()))
+data_3 = list(map(int, input('Введите числа через пробел ').split()))
+
+bubble_sort(data_1)
+selection_sort(data_2)
+insertion_sort(data_3)
+
+print('Пузырковая сортировка:', data_1)
+print('Сортировка выбором:', data_2)
+print('Сортировка вставкой:', data_3)
